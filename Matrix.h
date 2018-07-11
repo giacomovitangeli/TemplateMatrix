9549d56ae9bd6fe1cfdd657b3fede63165287019
Giacomo <giacomo.vitangeli@gmail.com>
2018-07-11 20:39:17 +0200

    fourth commit - Matrix product implementation, row and column getters

--- Matrix.h
+++ Matrix.h
@@ -8,11 +8,14 @@
 
 #include <iostream>
 #include <exception>
+#include <vector>
+#include <ctime>
 
 template<typename T>
 class Matrix {
 public:
-    Matrix(int w = 3, int h = 3) : width(w), height(h) {
+    Matrix(int h = 3, int w = 3, std::string n = "Default", std::string im = "auto") : height(h), width(w), name(n),
+                                                                                       initMode(im) {
         try {
             initMatrix();
         } catch (const std::out_of_range& e) {
@@ -21,7 +24,14 @@ public:
             width = 3;
             height = 3;
             buffer = new T[width*height];
-            setMatrixValues();
+            initNullMatrix();
+        } catch (const std::invalid_argument& e) {
+            e.what();
+            std::cout << "...Generating a default matrix..." << std::endl;
+            width = 3;
+            height = 3;
+            buffer = new T[width*height];
+            initNullMatrix();
         } catch (...) {}
     }
 
@@ -30,11 +40,43 @@ public:
     }
 
     void initMatrix() {
+        if (initMode == "manual")
+            initMatrixManually();
+        else if (initMode == "rauto")
+            initRandomMatrix();
+        else if (initMode == "auto")
+            initNullMatrix();
+        else
+            throw std::invalid_argument("Can't resolve the initialization mode selected!");
+    }
+
+    void initMatrixManually() {
         if (width <= 0 || height <= 0)
             throw std::out_of_range("Out of Range");
         else {
             buffer = new T[width*height];
             setMatrixValues();
+        }
+    }
+
+    void initRandomMatrix() {
+        if (width <= 0 || height <= 0)
+            throw std::out_of_range("Out of Range");
+        else {
+            srand(time(NULL));
+            buffer = new T[width*height];
+            for (int i = 0; i < (height*width); i++)
+                buffer[i] = rand()%100; //Init in the range 0 to 99
+        }
+    }
+
+    void initNullMatrix() {
+        if (width <= 0 || height <= 0)
+            throw std::out_of_range("Out of Range");
+        else {
+            buffer = new T[width*height];
+            for (int i = 0; i < (height*width); i++)
+                buffer[i] = 0;
         }
     }
 
@@ -49,7 +91,7 @@ public:
             throw std::out_of_range("Out of Range");
     }
 
-    Matrix& operator=(const Matrix& rhs) {
+    Matrix<T>& operator=(const Matrix& rhs) {
         if (this != &rhs) {
             if (buffer != nullptr)
                 delete[] buffer;
@@ -66,10 +108,58 @@ public:
         return *this;
     }
 
-    void printMatrix() {
-        std::cout << std::endl << "Print Matrix: ";
-        for (int i = 0; i < (width*height); i++) {
+    Matrix<T>& product(Matrix<T>& A, Matrix<T>& B) { //Product operator overloading to use it with template matrices
+        if (A.width == B.height) {
+            width = B.width;
+            height = A.height;
+            delete[] buffer;
+            buffer = new T[width*height];
 
+            int r = 0;
+            std::vector<T> rowA = A.getRow(A, r);
+            int c = 0;
+            std::vector<T> colB = B.getColumn(B, c);
+
+            for (int i = 0; i < (width*height); i++) {
+                if (c == A.height) {
+                    r++;
+                    rowA = A.getRow(A, r);
+                    c = 0;
+                    colB = B.getColumn(B, c);
+                }
+
+                for (int j = 0; j < A.width; j++)
+                    buffer[i] += rowA[j]*colB[j];
+
+                c++;
+                colB = B.getColumn(B, c);
+            }
+
+        } else
+            throw std::invalid_argument("Impossible to run the product between these two matrices");
+
+        return *this;
+    }
+
+    std::vector<T> getRow(const Matrix<T>& m, int r) { //Return a row of a matrix
+        std::vector<T> v;
+        for (int j = 0; j < m.width; j++) {
+            v.push_back(m.buffer[r*width+j]);
+        }
+        return v;
+    }
+
+    std::vector<T> getColumn(const Matrix<T>& m, int c) {  //Return a column of a matrix
+        std::vector<T> v;
+        for (int i = 0; i < height; i++) {
+            v.push_back(m.buffer[i*width+c]);
+        }
+        return v;
+    }
+
+    void printMatrix() {
+        std::cout << std::endl << "Print " << name << " Matrix: ";
+        for (int i = 0; i < (width*height); i++) {
             if (i%width == 0)
                 std::cout << std::endl;
             std::cout << buffer[i] << " ";
@@ -80,7 +170,7 @@ public:
     void setMatrixValues() {
         T value;
         std::cout << std::endl << "Setting the values of the matrix: " << std::endl;
-        for (int i = 0; i < (height); i++) {
+        for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
                 std::cout << "Matrix[" << i << ", " << j << "]: ";
                 std::cin >> value;
@@ -111,6 +201,7 @@ public:
 private:
     int width, height;
     T* buffer;
+    std::string name, initMode;
 };
 
 
--- main.cpp
+++ main.cpp
@@ -3,7 +3,25 @@
 #include "Matrix.h"
 
 int main() {
-    std::unique_ptr<Matrix<char>> m1(new Matrix<char>(2, 2));
+//    std::unique_ptr<Matrix<int>> A(new Matrix<int>(2, 2));
+/*DYNAMIC INIT
+    Matrix<int>* A = new Matrix<int>(2, 3, "A", "rauto");
+    A->printMatrix();
+    Matrix<int>* B = new Matrix<int>(3, 2, "B", "rauto");
+    B->printMatrix();
+    Matrix<int>* C = new Matrix<int>(2, 2, "C", "auto");
+    C->printMatrix();
+*/
+    //STATIC INIT
+    Matrix<int> A(2, 3, "A", "manual");
+    // A.printMatrix();
+    Matrix<int> B(3, 2, "B", "manual");
+    // B.printMatrix();
+    Matrix<int> C(10, 10, "C", "auto");
+    C.printMatrix();
+
+    C.product(A, B);
+    C.printMatrix();
 
     return 0;
 }
